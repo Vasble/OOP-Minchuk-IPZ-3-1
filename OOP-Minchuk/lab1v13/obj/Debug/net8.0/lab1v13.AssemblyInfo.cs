8584a93e0ff460e182d8f691cbbb5297399aff41
Vasble <mincukroman91@gmail.com>
2026-09-11 21:55:07 +0300

    photo

--- OOP-Minchuk/lab1v13/obj/Debug/net8.0/lab1v13.AssemblyInfo.cs
+++ OOP-Minchuk/lab1v13/obj/Debug/net8.0/lab1v13.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1v13")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e374dbf46c1c0e2538a20bc8e909488d908f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1v13")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1v13")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
